5271146c4917e3117c7216feb321fa3150783667
Muhsin Abboud <abboudmuhsin@gmail.com>
2025-06-17 21:43:09 +0200

    Update supabase client

--- supabase/functions/_shared/supabase-client.ts
+++ supabase/functions/_shared/supabase-client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/v135/@supabase/supabase-js@2.7.1/dist/module/index.js";
 
 export const supabaseClient = createClient(
-  Deno.env.get("MY_SUPABASE_URL") ?? "",
-  Deno.env.get("MY_SUPABASE_ANON_KEY") ?? "",
+  "https://" + (Deno.env.get("PROJECT_ID") ?? "") + ".supabase.co",
+  Deno.env.get("SUPABASE_ACCESS_TOKEN") ?? "",
 );
