4ea9a34469deb33d76d54c7deeee8ece6655a19e
Muhsin Abboud <abboudmuhsin@gmail.com>
2025-06-18 08:27:53 +0200

    Use supabase url from edge function secret

--- supabase/functions/_shared/supabase-client.ts
+++ supabase/functions/_shared/supabase-client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/v135/@supabase/supabase-js@2.7.1/dist/module/index.js";
 
 export const supabaseClient = createClient(
-  "https://" + (Deno.env.get("PROJECT_ID") ?? "") + ".supabase.co",
+  Deno.env.get("SUPABASE_URL") ?? "",
   Deno.env.get("SUPABASE_ANON_KEY") ?? "",
 );
