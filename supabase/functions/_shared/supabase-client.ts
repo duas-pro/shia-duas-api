bed49a599f7fbd3bfca0e6f01841bac0c107c210
Muhsin Abboud <abboudmuhsin@gmail.com>
2025-06-18 08:23:35 +0200

    Use supabase anon key from environment

--- supabase/functions/_shared/supabase-client.ts
+++ supabase/functions/_shared/supabase-client.ts
@@ -2,5 +2,5 @@ import { createClient } from "https://esm.sh/v135/@supabase/supabase-js@2.7.1/di
 
 export const supabaseClient = createClient(
   "https://" + (Deno.env.get("PROJECT_ID") ?? "") + ".supabase.co",
-  Deno.env.get("SUPABASE_ACCESS_TOKEN") ?? "",
+  Deno.env.get("SUPABASE_ANON_KEY") ?? "",
 );
