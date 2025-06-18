4b5844f4bc9235836f85a0f1345fc28a00c299a6
Muhsin Abboud <abboudmuhsin@gmail.com>
2025-06-18 20:15:12 +0200

    Add migration script to add dua view number and audio plays number

--- supabase/migrations/20250617200900_add_views_number.sql
+++ supabase/migrations/20250617200900_add_views_number.sql
@@ -1,3 +1,3 @@
-alter table "public"."duas" add column "page_views" number default 0;
+alter table "public"."duas" add column "page_views" int default 0;
 
-alter table "public"."dua_recitations" add column "audio_plays" number default 0;
+alter table "public"."dua_recitations" add column "audio_plays" int default 0;
