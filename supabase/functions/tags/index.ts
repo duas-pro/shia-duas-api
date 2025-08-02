f566ed1162f2fef6137da5217135a98ce702f3ea
Muhsin Abboud <abboudmuhsin@gmail.com>
2025-08-02 14:22:21 +0200

    Introduce new endpoint to get tags

---
+++ supabase/functions/tags/index.ts
@@ -0,0 +1,21 @@
+import { supabaseClient } from "../_shared/supabase-client.ts";
+import { createResponse } from "../_shared/response.ts";
+
+Deno.serve(async () => {
+  const excludedTags = ["sirat-guide"];
+
+  const { data, error } = await supabaseClient
+    .from("tags")
+    .select("name")
+    .not("name", "in", `(${excludedTags.join(",")})`)
+    .order("name", { ascending: true });
+
+  if (error) {
+    console.error(
+      "Error while fetching tags " + String(error?.message ?? error),
+    );
+    return createResponse(500, { error: `Internal Server Error` });
+  }
+
+  return createResponse(200, { success: true, data });
+});
